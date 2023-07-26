0876122738e0bf7089c5c574a911f0d91ed6eeff
Júlia Guimarães Franco <92822185+juliaguima@users.noreply.github.com>
2023-07-26 01:32:08 -0300

    BUSCAA, icons

--- home/views.py
+++ home/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render,redirect
 from home.models import Pesquisa
 from africa.models import model_africa
+from asia.models import model_asia
+from america.models import model_america
+from oceania.models import model_oceania
+from europa.models import model_europa
 
 def view_home(request):
     return render(request, 'home/paginas/index.html')
@@ -14,9 +18,15 @@ def view_busca(request):
         nome = request.GET['buscando']
         if nome:
             pesquisa = pesquisa.filter(nome__icontains=nome)
+    itens_filtro = []
+    itens_filtro.extend(model_africa.objects.filter(País__istartswith=nome))
+    itens_filtro.extend(model_america.objects.filter(País__istartswith=nome))
+    itens_filtro.extend(model_asia.objects.filter(País__istartswith=nome))
+    itens_filtro.extend(model_oceania.objects.filter(País__istartswith=nome))
+    itens_filtro.extend(model_europa.objects.filter(País__istartswith=nome))
 
-        if pesquisa == 'Africa':
-            return redirect('Africa:continente')
-            
-    return render(request, 'home/paginas/busca.html',context={'pesquisa':pesquisa})
-
+    return render(request, 'home/paginas/busca.html', context={
+        'pesquisa': pesquisa,
+        'itens_filtro': itens_filtro,
+        
+    })
